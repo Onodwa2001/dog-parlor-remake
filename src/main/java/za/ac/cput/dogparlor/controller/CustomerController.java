437eee71778cc9255e728deca8904f2743e14bcc
wendy <mawesh21@gmail.com>
2023-08-24 00:54:45 +0200

    commiting Address and AddressType domain

--- src/main/java/za/ac/cput/dogparlor/controller/CustomerController.java
+++ src/main/java/za/ac/cput/dogparlor/controller/CustomerController.java
@@ -2,7 +2,6 @@ package za.ac.cput.dogparlor.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import za.ac.cput.dogparlor.domain.Address;
 import za.ac.cput.dogparlor.domain.Contact;
 import za.ac.cput.dogparlor.domain.ContactType;
 import za.ac.cput.dogparlor.domain.Customer;
