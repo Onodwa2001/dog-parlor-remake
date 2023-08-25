51bd22c5409857e5511ce0e672b8957c34ebb817
youngLe0 <77040214+youngLe0@users.noreply.github.com>
2023-08-25 10:56:56 +0200

    Delete IContactService.java

--- src/main/java/za/ac/cput/dogparlor/service/IContactService.java
+++
@@ -1,9 +0,0 @@
-package za.ac.cput.dogparlor.service;
-
-import za.ac.cput.dogparlor.domain.Contact;
-
-import java.util.List;
-
-public interface IContactService extends IService<Contact, String> {
-    List<Contact> getAll();
-}
